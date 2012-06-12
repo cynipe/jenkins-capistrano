bcedc2ed4af50a64466083bb9ba5391d810bc510
cynipe <cynipe@gmail.com>
2012-06-13 00:50:11 +0900

    Add node api support to client

--- lib/jenkins-capistrano/client.rb
+++ lib/jenkins-capistrano/client.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'cgi/util'
+require 'json'
 
 module Jenkins
   class Client
@@ -37,6 +38,42 @@ module Jenkins
       end
     end
 
+    def add_node(name, opts = {})
+      options = default_node_options.merge(opts)
+      options[:name] = name
+      opstions[:labels] = options[:labels].split(/\s*,\s*/).join(' ') if options[:labels]
+
+      response = post_form("/computer/doCreateItem", node_form_fields(options))
+      case response
+      when Net::HTTPFound
+        { :name => opstions[:name], :slave_host => opstions[:slave_host] }
+      else
+        raise ServerError, parse_error_message(response)
+      end
+    end
+
+    def update_node(name, opts = {})
+      options = default_node_options.merge(opts)
+      options[:name] = name
+      opstions[:labels] = options[:labels].split(/\s*,\s*/).join(' ') if options[:labels]
+
+      response = post_form("/computer/#{CGI::escape(name)}/configSubmit", node_form_fields(options))
+      case response
+      when Net::HTTPFound
+        { :name => options[:name], :slave_host => options[:slave_host] }
+      else
+        raise ServerError, parse_error_message(response)
+      end
+    end
+
+    def config_node(name, opts = {})
+      begin
+        add_node(name, opts)
+      rescue ServerError => e
+        update_node(name, opts)
+      end
+    end
+
     private
     def xml_body(xml_str)
       {
@@ -52,6 +89,53 @@ module Jenkins
       error_msg = doc.search("td#main-panel p")
       error_msg.inner_text.empty? ? "Server error: code=#{response.code}, #{response.body}" : error_msg
     end
+
+    def post_form(path, fields)
+      url = URI.parse("#{self.class.base_uri}/#{path}")
+      req = Net::HTTP::Post.new(url.path)
+      req.set_form_data(fields)
+      http = Net::HTTP.new(url.host, url.port)
+      http.request(req)
+    end
+
+    def default_node_options
+      {
+        :slave_port  => 22,
+        :slave_user  => 'jenkins',
+        :master_key  => "/var/lib/jenkins/.ssh/id_rsa",
+        :slave_fs    => "/data/jenkins-slave/",
+        :description => "Automatically created by capistrano-jenkins",
+        :executors   => 2,
+        :exclusive   => true
+      }
+    end
+
+    def node_form_fields(options = {})
+      {
+        "name" => options[:name],
+        "type" => "hudson.slaves.DumbSlave$DescriptorImpl",
+        "json" => {
+          "name"              => options[:name],
+          "type"              => "hudson.slaves.DumbSlave$DescriptorImpl",
+          "nodeDescription"   => options[:description],
+          "numExecutors"      => options[:executors],
+          "remoteFS"          => options[:slave_fs],
+          "labelString"       => options[:labels],
+          "mode"              => options[:exclusive] ? "EXCLUSIVE" : "NORMAL",
+          "retentionStrategy" => { "stapler-class"  => "hudson.slaves.RetentionStrategy$Always" },
+          "nodeProperties"    => { "stapler-class-bag" => "true" },
+          "launcher"          => {
+            "stapler-class" => "hudson.plugins.sshslaves.SSHLauncher",
+            "host"          => options[:slave_host],
+            "port"          => options[:slave_port],
+            "username"      => options[:slave_user],
+            "privatekey"    => options[:master_key],
+          }
+        }.to_json
+      }
+    end
+
+
   end
 end
 
